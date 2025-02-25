8c837e31f09e4dd096a5955a05c45d44f6b73163
Brendan Dileo <dileob23@gmail.com>
2025-02-25 14:44:35 -0500

    Commit #3 - Setting up and Creating Basic Card Components

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+
+// Card.jsx
+// Brendan Dileo
+
+import '../components/Card.css';
+
+const Card = () => {
+    return (
+        <div className="card"></div>
+    )
+}
+
+// Exports
+export default Card;
