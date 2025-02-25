60d887b3fc0379d419c68f93aeeae0ef389f1301
Brendan Dileo <dileob23@gmail.com>
2025-02-25 14:45:45 -0500

    Commit #4 - Setting up and Creating Button Components

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+
+// Button.jsx
+// Brendan Dileo
+
+import "../components/Button.css"
+
+
+const Button = () => {
+    return (
+        <button></button>
+    )
+}
+
+// Export
+export default Button;
