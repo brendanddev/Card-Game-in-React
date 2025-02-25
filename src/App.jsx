3657c6da7b29e4de4e73365b5265da16635e6e16
Brendan Dileo <dileob23@gmail.com>
2025-02-25 15:23:01 -0500

    Commit #9 - Setting up the Deck Component

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,16 @@ function App() {
 
   return (
     <div className="main-container">
-    <h1>Assignment 3</h1>
-    <Card />
-    <Button label="Deal 5" className="btn-deal5" />
-    <Button label="Deal 7" className="btn-deal7" />
-    <Button label="Toss" className="btn-toss" />
-    <Button label="WildCard" className="btn-wildcard" />
-    <Button label="ReGroup" className="btn-regroup" />
-    <Button label="Reset" className="btn-reset" />
+      <h1>Assignment 3</h1>
+      <div className="card-container">
+      <Card />
+      </div>
+      <Button label="Deal 5" className="btn-deal5" />
+      <Button label="Deal 7" className="btn-deal7" />
+      <Button label="Toss" className="btn-toss" />
+      <Button label="WildCard" className="btn-wildcard" />
+      <Button label="ReGroup" className="btn-regroup" />
+      <Button label="Reset" className="btn-reset" />
     </div>
   )
 }
--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -4,8 +4,28 @@
 
 import "../components/Deck.css";
 
-
 const Deck = () => {
+    const suits = ["♥", "♦", "♣", "♠"];
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
+
+    
+    // Creates the deck
+    const createDeck = () => { }
+
+    const handleDeckClicked = () => { }
+
+    const handleCardClicked = () => { }
+
+    const resetDeck = () => { }
+    
+    const dealCards = ({ numOfCards }) => { }
+
+    const wildcardCard = () => { }
+
+    const tossCard = () => { }
+
+    const regroupCards = () => { }
+
 }
 
 // Exports the Deck component so it can be used other files
