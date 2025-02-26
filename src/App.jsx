d9988d0d75f377bcf6db13f254643d244c07cf75
Brendan Dileo <dileob23@gmail.com>
2025-02-25 20:44:23 -0500

    Commit #11 (Fix) - Deck Component Needed to be Displayed and not the Induidual Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,16 @@ function App() {
 
   return (
     <div className="main-container">
-      <h1>Assignment 3</h1>
+      <h1>Assignment 3 - Deck of Cards</h1>
       <div className="card-container">
-        <Card />
+        <Deck />
+      </div>
         <Button label="Deal 5" className="btn-deal5" />
         <Button label="Deal 7" className="btn-deal7" />
         <Button label="Toss" className="btn-toss" />
         <Button label="WildCard" className="btn-wildcard" />
         <Button label="ReGroup" className="btn-regroup" />
         <Button label="Reset" className="btn-reset" />
-      </div>
     </div>
   )
 }
--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -2,10 +2,7 @@
 // Deck.jsx
 // Brendan Dileo
 
-import { useState } from "react";
-import Card from "../components/Card";
 import "../components/Deck.css";
-
 
 const suits = ["♥", "♦", "♣", "♠"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
@@ -21,20 +18,14 @@ const createDeck = () => {
     return deck;
 }
 
+
 const Deck = () => {
-    const [deck, setDeck] = useState(createDeck());
-    const [dealtCards, setDealtCards] = useState([]);
 
     const handleDeckClicked = () => { 
         const randomCardIndex = Math.floor(Math.random() * deck.length);
         const card = deck[randomCardIndex];
-        setDealtCards([...dealtCards, card]);
     }
 
-    let cards = [];
-    dealtCards.forEach((card, index) => {
-        cards.push(<Card key={index} card={card} />);
-    });
 
     /*
         const handleCardClicked = () => { }
@@ -56,10 +47,7 @@ const Deck = () => {
                     ♧
                 </span>   
             </div>
-
-            <div className="card-list">
-                {cards}
-            </div>
+            
         </div>
     )
 
