66be5eccf58f34e238d62e48c5508823ea69e508
Brendan Dileo <dileob23@gmail.com>
2025-03-05 10:58:34 -0500

    Commit #19 - Creating a Hand Component for Modularity

---
+++ src/components/Hand.jsx
@@ -0,0 +1,22 @@
+
+// Hand.jsx
+// Brendan Dileo
+
+import Card from "../components/Card";
+import "../components/Deck.css";
+
+const Hand = ({ dealtCards }) => {
+    return (
+        <div className="card-list-container">
+            <h3>Your Hand</h3>
+            <div className="card-list">
+                {dealtCards.map((card, index) => (
+                    <Card key={index} card={card} />
+                ))}
+            </div>
+        </div>
+    )
+};
+
+export default Hand;
+
