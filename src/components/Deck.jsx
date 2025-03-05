bddedbffd2cbc38977f36307d64783c72f3b25cf
Brendan Dileo <dileob23@gmail.com>
2025-03-05 11:13:12 -0500

    Commit #20 (Issue) - No Cards show When Deck is Pressed

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -3,50 +3,82 @@
 // Brendan Dileo
 
 import { useState } from "react";
-import Card from "../components/Card";
+import { createDeck } from "../utils/manageDeck";
+
+import Hand from "../components/Hand";
 import "../components/Deck.css";
 
-const suits = ["♥", "♦", "♣", "♠"];
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
-
-// Creates the deck
-const createDeck = () => { 
-    let deck = [];
-    for (let suit of suits) {
-        for (let value of values) {
-            deck.push({ suit, value })
-        }
-    }
-    return deck;
-}
 
 const Deck = ({ dealtCards, setDealtCards }) => {
+
     const [deck, setDeck] = useState(createDeck());
     const [isDeckEmpty, setIsDeckEmpty] = useState(false);
 
+    const dealCards = (numOfCards) => {
+        if (deck.length === 0) {
+            setIsDeckEmpty(true);
+            console.log("Deck is empty!");
+            return;
+        }
+
+        let updatedDeck = [];
+        for (let i = 0; i < deck.length; i++) {
+            updatedDeck.push(deck[i]);
+        }
+
+        let newDealtCards = [];
+        let counter = 0;
+
+        while (counter < numOfCards && updatedDeck.length > 0) {
+            const randomCardIndex = Math.floor(Math.random() * updatedDeck.length);
+            newDealtCards.push(updatedDeck[randomCardIndex]);
+
+            let tempDeck = [];
+            for (let i = 0; i < updatedDeck.length; i++) {
+                if (i !== randomIndex) {
+                    tempDeck.push(updatedDeck[i]);
+                }
+            }
+            updatedDeck = tempDeck;
+            counter++;
+
+            if (i === numOfCards - 1 || updatedDeck.length === 0) {
+                setDeck(updatedDeck);
+                setDealtCards(newCards);
+                setIsDeckEmpty(updatedDeck.length === 0);
+            }
+        }
+    };
+
+   
     const handleDeckClicked = () => { 
         if (deck.length === 0) { 
             setIsDeckEmpty(true);
             console.log("Deck is empty"); 
             return; 
-        }
-        
-        const randomCardIndex = Math.floor(Math.random() * deck.length);
-        const card = deck[randomCardIndex];
 
-        const newDeck = [];
+        }
+
+        const randomCardIndex = Math.floor(Math.random() * deck.length);
+        const newDealtCard = deck[randomIndex];
+        let updatedDeck = [];
+
         for (let i = 0; i < deck.length; i++) {
             if (i !== randomCardIndex) {
-                newDeck.push(deck[i]);
+                updatedDeck.push(deck[i]);
             }
         }
-        setDeck(newDeck);
-        setDealtCards([...dealtCards, card]);
 
-        if (newDeck.length === 0) {
-            setIsDeckEmpty(true);
+        setDeck(updatedDeck);
+        let newDealtCards = [];
+        for (let i = 0; i < deck.length; i++) {
+            newDealtCards.push(dealtCards[i]);
         }
-    }
+
+        newDealtCard.push(newDealtCard);
+        setDealtCards(newDealtCards);
+        setIsDeckEmpty(updatedDeck.length === 0);
+    };
     
     return (
         <div className="deck-container">
@@ -66,14 +98,7 @@ const Deck = ({ dealtCards, setDealtCards }) => {
                     </div>
             )}
             <br />
-            <div className="card-list-container">
-                <h3>Your Hand</h3>
-                <div className="card-list">
-                    {dealtCards.map((card, index) => (
-                        <Card key={index} card={card} />
-                    ))}
-                </div>
-            </div>
+            <Hand  />
         </div>
     )
 
