586edc8d98bffdec7a9fef1650f5f2257514e8c2
Brendan Dileo <dileob23@gmail.com>
2025-03-05 13:18:51 -0500

    Commit #26 - Refactoring App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,18 @@
 // App.jsx
 // Brendan Dileo
 
-import { useState } from "react";
 import Deck from "../src/components/Deck";
-import Button from "./components/Button";
-
 import './App.css'
 
 function App() {
-  const [dealtCards, setDealtCards] = useState([]);
-  const [dealCards, setDealCards] = useState(() => () => {});
         
   return (
     <div className="main-container">
       <h1>Assignment 3 - Deck of Cards</h1>
       <div className="card-container">
         <Deck 
-          dealtCards={dealtCards} 
-          setDealtCards={setDealtCards} 
-          setDealCardsRef={setDealCards}
         />
       </div>
-        <Button label="Deal 5" className="btn-deal5" onClick={() => dealCards(5)}/>
-        <Button label="Deal 7" className="btn-deal7" onClick={() => dealCards(7)}/>
-        <Button label="Toss" className="btn-toss" />
-        <Button label="WildCard" className="btn-wildcard" />
-        <Button label="ReGroup" className="btn-regroup" />
-        <Button label="Reset" className="btn-reset" />
     </div>
   )
 }
