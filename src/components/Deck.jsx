55123da2e489ea317b90183ef474fec9468ffd59
Brendan Dileo <dileob23@gmail.com>
2025-03-05 23:03:29 -0500

    Commit #47 - Refactoring Utility File and Deck Component

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,18 +1,25 @@
 
-// Deck.jsx
-// Brendan Dileo
+/**
+ * @file Deck.jsx
+ * @author Brendan Dileo 
+ * 
+ * 
+ */
 
 import { useState } from "react";
-import { createDeck } from "../utils/manageDeck";
-
+import { createDeck, dealCardsFromDeck, shuffleCardsInHand, removeCardPermanent } from "../utils/manageDeck.js";
 import Button from "../components/Button";
 import Hand from "../components/Hand";
 import "../components/Deck.css";
 
 /**
+ * Functional Deck Component
  * 
- * @param {*} param0 
- * @returns 
+ * This component is responsible for managing the state of the deck, and the state of the cards that have been
+ * dealt. It will also make use of several utility functions to emphasize a seperation of concerns, and modular 
+ * code by using the functions in the manageDeck.js file.
+ * 
+ * @returns {JSX.Element} The JSX containing the rendered deck, and the controls associated with the deck.
  */
 const Deck = () => {
     const [deck, setDeck] = useState(createDeck());
@@ -27,19 +34,8 @@ const Deck = () => {
      * @param {*} numOfCards 
      */
     const dealCards = (numOfCards) => {
-        console.log("Dealing cards!");
-       let updatedDeck = [...deck, ...dealtCards];
-        setDeck(updatedDeck);
-        setDealtCards([]);
-
-        let newCards = [];
-        for (let i = 0; i < numOfCards; i++) {
-            if (updatedDeck.length === 0) break;
-            const randomCardIndex = Math.floor(Math.random() * updatedDeck.length);
-            newCards.push(updatedDeck[randomCardIndex]);
-            updatedDeck.splice(randomCardIndex, 1);
-        }
-
+        const { updatedDeck, newCards } = dealCardsFromDeck(deck, dealtCards, numOfCards);
+        
         setDealerMessage("Dealing your cards...");
         setTimeout(() => {
             setDeck(updatedDeck);
@@ -48,13 +44,12 @@ const Deck = () => {
         }, 750);
     };
 
-
     /**
      * 
      * @returns 
      */
     const handleDeckClicked = () => { 
-        console.log("Deck clicked!")
+        console.log("Deck clicked!");
         if (deck.length === 0) { 
             setIsDeckEmpty(true);
             console.log("Deck is empty"); 
@@ -93,56 +88,50 @@ const Deck = () => {
             setDealtCards(newDealtCards);
             setPickedCard(null);
         }
-    }
+    };
 
-    // Toss
-    const removeCardPermanent = () => {
+    /**
+     * 
+     * @returns 
+     */
+    const removeCardPermanentHandler = () => {
         console.log("Removing card... permanently!");
         if (pickedCard !== null) {
-            let newDealtCards = [...dealtCards];
-            let removedCard = newDealtCards.splice(pickedCard, 1)[0];
+            const { newDeck, newDealtCards, removedCard } = removeCardPermanent(deck, dealtCards, pickedCard);
 
             setDealtCards(newDealtCards);
             setPickedCard(null);
-            setRemovedCards(previousRemoved => [...previousRemoved, removedCard]);
-            setDeck(previousDeck => {
-                let newDeck = [];
-                for (let i = 0; i < previousDeck.length; i++) {
-                    const card = previousDeck[i];
-                    if (card.suit !== removedCard.suit || card.value !== removedCard.value) {
-                        newDeck.push(card);
-                    }
-                }
-                return newDeck;
-            });
+            setRemovedCards(prevRemoved => [...prevRemoved, removedCard]);
+            setDeck(newDeck);
         } else {
             alert("No card is selected! Please make a selection first!");
             return;
         }
-    }
+    };
 
-    // Regroup
-    const shuffleCardsInHand = () => {
-        if (dealCards.length === 0) {
+    /**
+     * 
+     * @returns 
+     */
+    const shuffleCardsInHandHandler = () => {
+        if (dealtCards.length === 0) {
             alert("There are no cards in your hand! Deal some cards before shuffling!");
             return;
         }
 
         console.log("Shuffling cards in hand!");
-        let shuffledCardsInHand = [...dealtCards];
-        for (let i = shuffledCardsInHand.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffledCardsInHand[i], shuffledCardsInHand[j]] = [shuffledCardsInHand[j], shuffledCardsInHand[i]];
-        }
+        const shuffledCardsInHand = shuffleCardsInHand(dealtCards);
         setDealtCards(shuffledCardsInHand);
-    }
+    };
 
-    // Wildcard
+    /**
+     * 
+     */
     const createRandomCard = () => {
         const fullDeck = createDeck();
         const randomCard = fullDeck[Math.floor(Math.random() * fullDeck.length)];
         setDealtCards([...dealtCards, randomCard]);
-    }  
+    };
 
     /**
      * 
@@ -153,23 +142,14 @@ const Deck = () => {
 
         for (let i = 0; i < newDeck.length; i++) {
             const card = newDeck[i];
-
-            let isCardRemoved = false;
-            for (let j = 0; j < removedCards.length; j++) {
-                const removedCard = removedCards[j];
-                if (removedCard.suit === card.suit && removedCard.value === card.value) {
-                    isCardRemoved = true;
-                    break;
-                }
-            }
+            let isCardRemoved = removedCards.some(removedCard => removedCard.suit === card.suit && removedCard.value === card.value);
             if (!isCardRemoved) filteredDeck.push(card);
         }
 
         setDeck(filteredDeck);
         setDealtCards([]);
         setIsDeckEmpty(false);
-        console.log("Deck has been reset!");
-    }
+    };
 
     return (
         <div className="deck-container">
@@ -179,8 +159,7 @@ const Deck = () => {
                 </div>
             ) : (
                 <>
-
-                <h1 className="mt-4 mb-10 text-2xl font-bold border-b border-black pb-2">The Deck O' Cards</h1>
+                    <h1 className="mt-4 mb-10 text-2xl font-bold border-b border-black pb-2">The Deck O' Cards</h1>
                     <div className="deck" onClick={handleDeckClicked}>
                         <div className="deck-list">
                             <div className="deck-card deck-card-one"></div>
@@ -201,42 +180,39 @@ const Deck = () => {
             )}
 
             <div className="button-container">
-                    <Button 
-                        label="Deal 5" 
-                        className="bg-blue-500 text-white hover:bg-blue-600"
-                        onClick={() => dealCards(5)}
-                    />
-                    <Button 
-                        label="Deal 7" 
-                        className="bg-red-500 text-white hover:bg-red-600"
-                        onClick={() => dealCards(7)}
-                    />
-                    <Button 
-                        label="Toss" 
-                        className="bg-red-500 text-white hover:bg-red-600"
-                        onClick={removeCardPermanent} 
-                    />
-                    <Button 
-                        label="ReGroup" 
-                        className="bg-purple-500 text-white hover:bg-purple-600"
-                        onClick={shuffleCardsInHand}
-                    />
-                    <Button 
-                        label="WildCard" 
-                        className="bg-yellow-500 text-black hover:bg-yellow-600"
-                        onClick={createRandomCard}
-                    />
-                    <Button 
-                        label="Reset" 
-                        className="bg-cyan-500 text-white hover:bg-cyan-600"
-                        onClick={() => resetDeck()}
-                        
-                    />
+                <Button 
+                    label="Deal 5" 
+                    className="bg-blue-500 text-white hover:bg-blue-600"
+                    onClick={() => dealCards(5)}
+                />
+                <Button 
+                    label="Deal 7" 
+                    className="bg-red-500 text-white hover:bg-red-600"
+                    onClick={() => dealCards(7)}
+                />
+                <Button 
+                    label="Toss" 
+                    className="bg-red-500 text-white hover:bg-red-600"
+                    onClick={removeCardPermanentHandler} 
+                />
+                <Button 
+                    label="ReGroup" 
+                    className="bg-purple-500 text-white hover:bg-purple-600"
+                    onClick={shuffleCardsInHandHandler}
+                />
+                <Button 
+                    label="WildCard" 
+                    className="bg-yellow-500 text-black hover:bg-yellow-600"
+                    onClick={createRandomCard}
+                />
+                <Button 
+                    label="Reset" 
+                    className="bg-cyan-500 text-white hover:bg-cyan-600"
+                    onClick={() => resetDeck()}
+                />
             </div>
         </div>
     );
 };
 
-
-// Exports the Deck component so it can be used other files
 export default Deck;
--- src/utils/manageDeck.js
+++ src/utils/manageDeck.js
@@ -1,14 +1,24 @@
 
-// manageDeck.js
-// Brendan Dileo
-// A utility file to manage the deck of cards, and seperate the logic from the Deck
-
+/**
+ * @file manageDeck.js
+ * @author Brendan Dileo 
+ * 
+ * This is a utility file created to structure, create, and manage the deck of cards.
+ * The goal was to move any logic from Deck.jsx to this file, to imrpove the readability
+ * and modularity of the Deck component. THis utility file contains functions for creating
+ * a deck of cards, shuffling the cards, and permanently removing a card.
+ */
 
 const suits = ["♥", "♦", "♣", "♠"];
 const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 
-// Creates the deck
+/**
+ * This function creates a deck of 52 cards, no jokers are included.
+ * Each of the cards created for the deck will have a corresponding suit and a pair.
+ * 
+ * @returns {Array} - A deck of 52 card objects, each with a suit and value property.
+ */
 export const createDeck = () => { 
     let deck = [];
     for (let suit of suits) {
@@ -17,4 +27,67 @@ export const createDeck = () => {
         }
     }
     return deck;
+};
+
+/**
+ * This function is responsible for dealing a specified number of cards from the deck (5 or 7).
+ * The cards are chosen randomly from the deck by making use of the 'Math' class, and are stored
+ * into a new array containing the cards being dealt.
+ * 
+ * @param {Array} deck - The deck of card objects.
+ * @param {Array} dealtCards - The cards that have been already dealt.
+ * @param {int} numOfCards - The number of cards to be dealt.
+ * @returns {Object} - An object containing the updated deck and an array of the cards dealt.
+ */
+export const dealCardsFromDeck = (deck, dealtCards, numOfCards) => {
+    let updatedDeck = [...deck, ...dealtCards];
+    let newCards = [];
+    for (let i = 0; i < numOfCards; i++) {
+        if (updatedDeck.length === 0) break;
+        const randomCardIndex = Math.floor(Math.random() * updatedDeck.length);
+        newCards.push(updatedDeck[randomCardIndex]);
+        // Removes the dealt card from the deck
+        updatedDeck.splice(randomCardIndex, 1);
+    }
+    return { updatedDeck, newCards };
+};
+
+/**
+ * This function will shuffle the cards that are in the users hand using a Fisher Yates algorithm.
+ * 
+ * @param {Array} cards - The cards to shuffle in the users hand.
+ * @returns {Array} - An shuffled array of the cards.
+ */
+export const shuffleCardsInHand = (cards) => {
+    let shuffledCards = [...cards];
+    // Fisher yates
+    for (let i = shuffledCards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledCards[i], shuffledCards[j]] = [shuffledCards[j], shuffledCards[i]];
+    }
+    return shuffledCards;
+};
+
+/**
+ * This function permanently removes a card from the list of cards dealt, and the deck of cards.
+ * The 'picked' card is identified by its index in the array of cards, and then spliced to be 
+ * removed from the array of cards dealt. It is then filtered out of the deck itself so even
+ * after resetting this card will not reappear.
+ * 
+ * Although it is not necesarily clear what is asked of this in the assignment requirements, I 
+ * interpreted it as essentially swapping the picked card with the next card, or un selecting the
+ * picked card. If the page is reset, this will be overriden.
+ * 
+ * @param {Array} deck - The current deck of cards.
+ * @param {Array} dealtCards - The array of cards that have already been dealt.
+ * @param {int} pickedCard - The index of the selected card that will be 'tossed' (removed).
+ * @returns {Object} - An object containing the updated deck, the updated cards dealt, and the card that has been removed.
+ */
+export const removeCardPermanent = (deck, dealtCards, pickedCard) => {
+    let newDealtCards = [...dealtCards];
+    let removedCard = newDealtCards.splice(pickedCard, 1)[0];
+    
+    let newDeck = deck.filter(card => !(card.suit === removedCard.suit && card.value === removedCard.value));
+    
+    return { newDeck, newDealtCards, removedCard };
 };
