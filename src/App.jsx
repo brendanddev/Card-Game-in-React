e88b765e56dbc19a6ed4892503b0a1ad0aa0bc0a
Brendan Dileo <dileob23@gmail.com>
2025-03-06 12:08:02 -0500

    Commit #51 - Finalizing Styling

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,12 @@ import './App.css'
 function App() {
   return (
       <div className="main-container">
-          <h1 class="text-5xl font-extrabold italic my-6 text-gray-800">Assignment 3 - Deck of Cards</h1>
-          <div className="card-container">
-              <Deck />
-          </div>
+         <h1 className="text-5xl font-extrabold italic my-6 text-gray-800 transform transition duration-300 hover:scale-105">
+            Assignment 3 - Deck of Cards
+        </h1>
+        <div className="card-container">
+            <Deck />
+        </div>
       </div>
   )
 }
