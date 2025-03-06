11fd424d273837213d0b33571a0f7b846026bdab
Brendan Dileo <dileob23@gmail.com>
2025-03-05 22:31:04 -0500

    Commit #46 - Adding Documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 
-// App.jsx
-// Brendan Dileo
+
+/**
+ * @file App.jsx
+ * @author Brendan Dileo
+ * 
+ * 
+ * The root of the React Card Game application.
+ * This component is the main container for the application, and will render the title of the page,
+ * and the Deck component, which will handle majority of the game logic, and render the deck onto
+ * the screen.
+ */
 
 import Deck from "../src/components/Deck";
 import './App.css'
 
+/**
+ * App Component
+ * 
+ * Renders the title of the page, and the Deck component.
+ * 
+ * @returns {JSX.Element} The JSX containing the rendered main container.
+ */
 function App() {
-        
   return (
     <div className="main-container">
         <h1 class="text-5xl font-extrabold italic my-6 text-gray-800">Assignment 3 - Deck of Cards</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,13 @@
+
+/**
+ * @file main.jsx 
+ * @author Brendan Dileo
+ * 
+ * The entry point to the React Card Game application.
+ * This file has not been edited, but will render the root component into the DOM.
+ * The 'index.css' file is imported to allow the entire project to use the TatilwindCSS styles.
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
